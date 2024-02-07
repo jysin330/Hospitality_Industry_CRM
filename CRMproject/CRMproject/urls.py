81a661f14e3e6fe46624a73b823ae7c212e0c3a6
Jyoti <jyotisingh82000@gmail.com>
2024-02-07 13:54:38 +0530

    urls.py setting

--- CRMproject/CRMproject/urls.py
+++ CRMproject/CRMproject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from CRMapp.views import MetricsView
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
+    
+    path('metrics/', MetricsView.as_view(), name='metrics'),
 ]
