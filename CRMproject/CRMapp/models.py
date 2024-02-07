d5c46a134d39526bb575b7fdaff82a87afd17b45
Jyoti <jyotisingh82000@gmail.com>
2024-02-07 13:12:34 +0530

    Create DailyTotalSummary model in CRMapp

--- CRMproject/CRMapp/models.py
+++ CRMproject/CRMapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DailyTotalSummary(models.Model):
+    city = models.CharField(max_length=50)
+    net_sale = models.IntegerField()
+    net_expense = models.IntegerField()
+    daily_target = models.IntegerField()
+    date = models.DateField()
+
